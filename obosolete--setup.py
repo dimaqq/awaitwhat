34090df32e5586ccd7472fbd3a163065bd9f8fca
Dima Tisnek <dimaqq@gmail.com>
2019-08-27 17:24:03 +0900

    cleanup

--- obosolete--setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-helper = Extension('what',
-                    sources = ['what.c'])
-
-setup (name = 'Await What?',
-       version = '0.0',
-       description = 'Await What?',
-       ext_modules = [helper])
