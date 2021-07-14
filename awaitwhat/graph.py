0d0990b3384c12da51bab2041202361156e23c4f
Puneethnaik <puneethn250@gmail.com>
2021-07-14 08:30:26 +0530

    Traverse asyncio.wait_for #5 (#31)
    * traverse wait_for (show awaitable behind it) and report remaining timeout

--- awaitwhat/graph.py
+++ awaitwhat/graph.py
@@ -19,6 +19,19 @@ def new(tasks) -> Tuple[Set[Vertex], List[Edge]]:
     for who in tasks:
         src = Vertex.new(who, current)
         vertices.add(src)
+        children = blockers(who)
+        if len(children) > 1 and isinstance(children[1], asyncio.Task):
+            # This is a wait_for task
+            # We get two vertices, one for wait_for(dst1)
+            # and other for the awaitable that wait_for 
+            # awaits on(dst2)
+            dst1 = Vertex.new(children[0], current)
+            dst2 = Vertex.new(children[1], current)
+            vertices.add(dst1)
+            vertices.add(dst2)
+            edges.append(Edge(src, dst1))
+            edges.append(Edge(dst1, dst2))
+            continue
         for what in blockers(who):
             dst = Vertex.new(what, current)
             vertices.add(dst)
