936b1f14294ea13ebbb51f1ad62679e76d7e47a3
Dima Tisnek <dimaqq@gmail.com>
2021-07-14 14:45:44 +0900

    fix #33 graphviz alignment

--- awaitwhat/dot.py
+++ awaitwhat/dot.py
@@ -6,6 +6,8 @@ from . import graph
 def label(vertex):
     """Graph vertex label in dot format"""
     label = f"{vertex.name} {vertex.state or ''}\n{vertex.traceback or ''}"
+    if not label.endswith("\n"):
+        label += "\n"
     label = json.dumps(label).replace("\\n", r"\l")
     return f"[label={label}]"
 
